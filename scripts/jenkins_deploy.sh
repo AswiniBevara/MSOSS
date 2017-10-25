c365d23a8f0c45d17568c0d45fd7fb160f71c794
ashwinse <asebastian@sysgain.com>
2017-10-25 12:34:03 +0530

    up

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -65,7 +65,7 @@ userName = &quot;${13}&quot;
 password = &quot;${14}&quot;
 _artifactsLocation = &quot;${22}&quot; 
 kibanaUsername = &quot;${23}&quot;
-kibanaPassword = &quot;${23}&quot;" $srcdir/elk-config.xml | sed "s/&amp;quot;/\"/g" > $srcdir/elk-newconfig.xml
+kibanaPassword = &quot;${24}&quot;" $srcdir/elk-config.xml | sed "s/&amp;quot;/\"/g" > $srcdir/elk-newconfig.xml
 fi
 
 if [ ! -f "MongoDBTerraformjob.xml" ]
