c2c9fe3c32e53c69fcca0f25cc9850bd3d5c2030
nvtuluva <nvtuluva@sysgain.com>
2017-10-28 00:39:44 +0530

    Update filebeat.sh

--- scripts/filebeat.sh
+++ scripts/filebeat.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 sudo wget $1/scripts/filebeat.yml
-touch /home/adminuser/filebeatnew.yml
+touch /home/$3/filebeatnew.yml
 sudo cat /etc/filebeat/filebeat.yml | sed "s/ \[\"localhost/ \[\"$2/g" >/home/adminuser/filebeatnew.yml
 sudo cp /home/adminuser/filebeatnew.yml /etc/filebeat/filebeat.yml
 sudo service filebeat restart
