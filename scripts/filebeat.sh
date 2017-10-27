e7c69f2b1e295a24ffbbc2192b72f8a76dd97e61
unknown <srekapalli@sysgain.com>
2017-10-27 17:41:05 +0530

    update

--- scripts/filebeat.sh
+++ scripts/filebeat.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/kubstage/scripts/filebeat.yml
+sudo wget $1/scripts/filebeat.yml
 touch /home/adminuser/filebeatnew.yml
-sudo cat /etc/filebeat/filebeat.yml | sed "s/ \[\"localhost/ \[\"$1/g" >/home/adminuser/filebeatnew.yml
+sudo cat /etc/filebeat/filebeat.yml | sed "s/ \[\"localhost/ \[\"$2/g" >/home/adminuser/filebeatnew.yml
 sudo cp /home/adminuser/filebeatnew.yml /etc/filebeat/filebeat.yml
 sudo service filebeat restart
 sudo service filebeat status
